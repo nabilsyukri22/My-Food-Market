7f7beab1b83d71dada1123e53ef5831fd79a2aa4
nabilsyukri22 <73006496+nabilsyukri22@users.noreply.github.com>
2021-10-27 11:36:14 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/myfoodmarket/ui/auth/signup/SignupSuccessFragment.kt
@@ -0,0 +1,21 @@
+package com.myfoodmarket.ui.auth.signup
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myfoodmarket.R
+import kotlinx.android.synthetic.main.fragment_signup_success.*
+
+class SignupSuccessFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup_success, container, false)
+    }
+}
